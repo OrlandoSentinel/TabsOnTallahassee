591429f9fe3cd9beeb2290739b6467930f21b758
James Turk <james.p.turk@gmail.com>
2015-09-12 12:54:37 -0400

    fix process_list for contact details

--- fl/people.py
+++ fl/people.py
@@ -2,6 +2,8 @@ from pupa.scrape import Scraper, Person
 import lxml.html
 import re
 
+class NoListItems(Exception):
+    pass
 
 class Page:
     def __init__(self, scraper, url, *, list_xpath=None, obj=None):
@@ -15,11 +17,23 @@ class Page:
     def process_list_item(self, item):
         pass
 
-    def process_list(self):
+    def yield_list(self):
+        n = 0
         for item in self.doc.xpath(self.list_xpath):
+            n += 1
             processed = self.process_list_item(item)
             if processed:
                 yield processed
+        if not n:
+            raise NoListItems()
+
+    def process_list(self):
+        n = 0
+        for item in self.doc.xpath(self.list_xpath):
+            n += 1
+            processed = self.process_list_item(item)
+        if not n:
+            raise NoListItems()
 
 
 class SenDetail(Page):
@@ -33,26 +47,31 @@ class SenDetail(Page):
         else:
             type_ = 'district'
 
-        # phone
-        PHONE_RE = r'\(\d{3}\)\s\d{3}\-\d{4}'
-        if re.search(PHONE_RE, address_lines[-1]):
-            phone = address_lines.pop()
-        else:
-            phone = None
-        if phone:
-            self.obj.add_contact_detail(type='phone', value=phone, note=type_)
-
-        # address
         address_lines = [x.strip() for x in
                          office.xpath('following-sibling::div[1]/text()')
                          if x.strip()]
-        if re.search(r'(?i)open\s+\w+day', address_lines[0]):
-            address_lines = address_lines[1: ]
-        assert ", FL" in address_lines[-1]
-        address = "\n".join(address_lines)
-        address = re.sub(r'\s{2,}', " ", address)
 
-        self.obj.add_contact_detail(type='address', value=address, note=type_)
+        clean_address_lines = []
+        phone = None
+        PHONE_RE = r'\(\d{3}\)\s\d{3}\-\d{4}'
+
+        for line in address_lines:
+            if re.search(r'(?i)open\s+\w+day', address_lines[0]):
+                continue
+            elif re.search(PHONE_RE, line):
+                phone = line
+                break
+            else:
+                clean_address_lines.append(line)
+
+        if phone:
+            self.obj.add_contact_detail(type='voice', value=phone, note=type_)
+
+        # address
+        address = "\n".join(clean_address_lines)
+        address = re.sub(r'\s{2,}', " ", address)
+        if address:
+            self.obj.add_contact_detail(type='address', value=address, note=type_)
 
     def process_page(self):
         email = self.doc.xpath('//a[contains(@href, "mailto:")]')[0].get('href').split(':')[-1]
@@ -76,14 +95,12 @@ class SenList(Page):
 
         district = item.xpath("string(../../td[1])")
         party = item.xpath("string(../../td[2])")
-
-        # for consistency
-        #if party == 'Democrat':
-        #    party = 'Democratic'
+        if party == 'Democrat':
+            party = 'Democratic'
 
         leg_url = item.get('href')
 
-        leg = Person(name=name, district=district, party=party, primary_org='upper')
+        leg = Person(name=name, district=district, party=party, primary_org='upper', role='Senator')
         leg.add_link(leg_url)
         leg.add_source(self.url)
         leg.add_source(leg_url)
@@ -99,4 +116,4 @@ class FlPersonScraper(Scraper):
 
     def scrape(self):
         sl = SenList(self)
-        yield from sl.process_list()
+        yield from sl.yield_list()
