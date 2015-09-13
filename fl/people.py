461f48da39d6fd2e095c78c3cc30d1256ea95d75
James Turk <james.p.turk@gmail.com>
2015-09-12 20:25:25 -0400

    representative scraper

--- fl/people.py
+++ fl/people.py
@@ -1,9 +1,11 @@
 from pupa.scrape import Scraper, Person
+from urllib import parse
 import lxml.html
 import re
 
 class NoListItems(Exception):
     pass
+
 
 class Page:
     def __init__(self, scraper, url, *, list_xpath=None, obj=None):
@@ -105,11 +107,73 @@ class SenList(Page):
         leg.add_source(self.url)
         leg.add_source(leg_url)
 
-        sd_obj = SenDetail(self.scraper, leg_url, obj=leg)
-        sd_obj.process_list()
-        sd_obj.process_page()
+        sd = SenDetail(self.scraper, leg_url, obj=leg)
+        sd.process_list()
+        sd.process_page()
 
         return leg
+
+
+class RepList(Page):
+    def __init__(self, scraper):
+        super().__init__(scraper,
+                         "http://www.flhouse.gov/Sections/Representatives/representatives.aspx",
+                         list_xpath='//div[@id="MemberListing"]/div[@class="rep_listing1"]')
+
+
+    def process_list_item(self, item):
+        link = item.xpath('.//div[@class="rep_style"]/a')[0]
+        name = link.text_content().strip()
+
+        if 'Vacant' in name or 'Resigned' in name or 'Pending' in name:
+            return
+
+        party = item.xpath('.//div[@class="party_style"]/text()')[0].strip()
+        party = {'D': 'Democratic', 'R': 'Republican'}[party]
+
+        district = item.xpath('.//div[@class="district_style"]/text()')[0].strip()
+
+        leg_url = link.get('href')
+        split_url = parse.urlsplit(leg_url)
+        member_id = parse.parse_qs(split_url.query)['MemberId'][0]
+        image = "http://www.flhouse.gov/FileStores/Web/Imaging/Member/{}.jpg".format(member_id)
+
+        rep = Person(name=name, district=district, party=party, primary_org='lower', role='Representative')
+        rep.add_link(leg_url)
+        rep.add_source(leg_url)
+        rep.add_source(self.url)
+
+        rd = RepDetail(self.scraper, leg_url, obj=rep)
+        rd.scrape_office('Capitol Office')
+        rd.scrape_office('District Office')
+
+        return rep
+
+
+class RepDetail(Page):
+    def scrape_office(self, name):
+        pieces = [x.tail.strip() for x in self.doc.xpath('//strong[text()="{}"]/following-sibling::br'.format(name))]
+
+        if not pieces:
+            # TODO: warn?
+            return
+        address = []
+
+        for piece in pieces:
+            if piece.startswith('Phone:'):
+                # Phone: \r\n     (303) 111-2222
+                if re.search(r'\d+', piece):
+                    phone = piece.split(None, 1)[1]
+                else:
+                    phone = None
+            else:
+                address.append(re.sub(r'\s+', ' ', piece))
+
+        type_ = 'capitol' if 'Capitol' in name else 'district'
+
+        self.obj.add_contact_detail(type='address', value='\n'.join(address), note=type_)
+        if phone:
+            self.obj.add_contact_detail(type='voice', value=phone, note=type_)
 
 
 class FlPersonScraper(Scraper):
@@ -117,3 +181,5 @@ class FlPersonScraper(Scraper):
     def scrape(self):
         sl = SenList(self)
         yield from sl.yield_list()
+        rl = RepList(self)
+        yield from rl.yield_list()
