43dbc0c9ed013177f921b4c18a6e8561fd47f7c7
James Turk <james.p.turk@gmail.com>
2015-09-15 17:55:10 -0400

    summary of what is going on here

--- fl/base.py
+++ fl/base.py
@@ -1,3 +1,42 @@
+"""
+    "Spatula"
+
+    An experiment in making scrapers easier to maintain and semi-testable.
+
+    General premise is that scraping is broken down into page-by-page tasks.
+
+    Each type of page encountered by the scraper is represented by a subclass of
+    ``Page``
+
+    99% of scraper code either goes in handle_list_item (for cases where
+    list of similar objects is being scraped, like a table) or in
+    handle_page if a similar item is being scraped.
+
+    So if there were a list of legislators and a detail page for each one might
+    make LegislatorList and LegislatorDetail classes.
+
+    LegislatorList could likely get by with just handle_list_item overridden.
+
+    LegislatorDetail likely with just handle_page.
+
+    Usage looks something like:
+        yield from scraper.scrape_page_items(LegislatorList)
+
+    And within LegislatorList.handle_list_item something similar to:
+        self.scrape_page(LegislatorDetail, url=legislator_url, obj=leg)
+
+
+    A few notes:
+        ``url`` can either be passed in to scrape_page/scrape_page_items or set on the class
+        itself in cases where it is static (like list pages).
+
+        the ``obj`` parameter allows a partially-completed object to be
+        filled out by a detail page
+
+        Additional parameters can be passed into the scrape_* methods and will
+        be available as extras.
+            (TODO: should these chain?  refine API for accessing these beyond kwargs.get?)
+"""
 import os
 import subprocess
 import lxml.html
@@ -40,8 +79,6 @@ class Page(AbstractPage):
             creates an instance of ``page_type`` that knows about an object
             being built (``obj``)
         """
-        if not obj:
-            raise ValueError('must pass obj to scrape_page')
         page_type(self.scraper, url=url, obj=obj, **kwargs).handle_page()
 
     def handle_list_item(self, item):
