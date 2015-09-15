49f204c015d370eca7af5b8ad6de28cc9c3df2fb
James Turk <james.p.turk@gmail.com>
2015-09-14 22:04:02 -0400

    add PDF base

--- fl/base.py
+++ fl/base.py
@@ -1,3 +1,5 @@
+import os
+import subprocess
 import lxml.html
 
 
@@ -35,3 +37,31 @@ class Page:
             self.process_list_item(item)
         if not n:
             raise NoListItems()
+
+
+class PDF:
+    def __init__(self, scraper, url=None, *, obj=None, **kwargs):
+        self.scraper = scraper
+        if url:
+            self.url = url
+        self.obj = obj
+        self.kwargs = kwargs
+
+        (path, resp) = self.scraper.urlretrieve(url)
+
+        self.text = self.convert_pdf(path, 'text').decode('utf8')
+        self.lines = self.text.split('\n')
+        os.remove(path)
+
+    def convert_pdf(self, filename, type='xml'):
+        commands = {'text': ['pdftotext', '-layout', filename, '-'],
+                    'text-nolayout': ['pdftotext', filename, '-'],
+                    'xml': ['pdftohtml', '-xml', '-stdout', filename],
+                    'html': ['pdftohtml', '-stdout', filename]}
+        try:
+            pipe = subprocess.Popen(commands[type], stdout=subprocess.PIPE, close_fds=True).stdout
+        except OSError as e:
+            raise EnvironmentError("error running {}, missing executable? [{}]".format(' '.join(commands[type]), e))
+        data = pipe.read()
+        pipe.close()
+        return data
