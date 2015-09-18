9244d6c0292e694901a7308d04b2dca7c70448c1
erinspace <erin.braswell@gmail.com>
2015-09-17 22:44:28 -0400

    Add registration urls to main urlconf

--- tot/urls.py
+++ tot/urls.py
@@ -20,7 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^glossary/', include('glossary.urls')),
-    url('^', include('django.contrib.auth.urls'))
+    url(r'^accounts/', include('registration.backends.default.urls'))
 ]
 
 urlpatterns += staticfiles_urlpatterns()
