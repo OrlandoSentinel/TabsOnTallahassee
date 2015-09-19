091a08a8fe30c3cc00c6b85552e47a1b15b807b8
erinspace <erin.braswell@gmail.com>
2015-09-19 18:02:10 -0400

    Add userprefs and email reg view

--- preferences/views.py
+++ preferences/views.py
@@ -1,10 +1,21 @@
 from django.shortcuts import render
+from django.views.generic.edit import FormView
 
-# Create your views here.
-
-from registration.views import RegistrationView
 from registration.forms import RegistrationFormUniqueEmail
+from registration.backends.default.views import RegistrationView
+
+from preferences.forms import PreferencesForm
+
 
 class EmailRegistrationView(RegistrationView):
 
     form_class = RegistrationFormUniqueEmail
+
+
+class UserPreferences(FormView):
+    template_name = 'preferences/preferences.html'
+    form_class = PreferencesForm
+    success_url = '/index/'
+
+    def form_valid(self, form):
+        return super(UserPreferences, self).form_valid(form)
