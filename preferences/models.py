1cb0cb3167a9d641c45d034000a9b0f5202c0dde
erinspace <erin.braswell@gmail.com>
2015-09-20 19:17:39 -0400

    Add preferences model with address rep and sen

--- preferences/models.py
+++ preferences/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from opencivicdata.models.people_orgs import Person
+
+
+class Preferences(models.Model):
+    representitive = models.ForeignKey(Person, related_name='rep_preferences')
+    senator = models.ForeignKey(Person, related_name='sen_preferences')
+
+    street_line1 = models.CharField(max_length = 100, blank = True)
+    street_line2 = models.CharField(max_length = 100, blank = True)
+    zipcode = models.CharField(max_length = 5, blank = True)
+    city = models.CharField(max_length = 100, blank = True)
+    state = models.CharField(max_length = 100, blank = True)
