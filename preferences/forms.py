52ed2401fedf5d477c423095954bc973d396f88d
erinspace <erin.braswell@gmail.com>
2015-09-24 17:30:30 -0400

    Add a filter that does not quite work for preferences form

--- preferences/forms.py
+++ preferences/forms.py
@@ -6,33 +6,12 @@ from preferences.models import Preferences
 from opencivicdata.models.people_orgs import Person
 
 
-# from django.forms import ModelForm
-
-# class RegistrationUserForm(RegistrationFormUniqueEmail):
-
-#     class Meta:
-#         model = User
-#         fields = ("email")
-
-
-# class PreferencesForm(forms.ModelForm):
-#     class Meta:
-#         model = Preferences
-#         fields = ['representitive', 'senator', 'street_line1', 'street_line2',
-#                   'zipcode', 'city', 'state']
-
-#         def __init__(self, **kwargs):
-#                 super(PreferencesForm, self).__init__(**kwargs)
-#                 self.fields['senator'].queryset = Preferences.objects.filter(memberships='Florida: Florida Senate')
-
-
 class CustomModelFilter(forms.ModelChoiceField):
     def label_from_instance(self, obj):
-        # import ipdb; ipdb.set_trace()
         return "%s" % (obj.name)
 
 class PreferencesForm(forms.ModelForm):
-    senator = CustomModelFilter(queryset=Person.objects.filter(name='Aaron Bean'))
+    senator = CustomModelFilter(queryset=Person.objects.filter(memberships__organization='Florida Senate'))
 
     class Meta:
         model = Preferences
