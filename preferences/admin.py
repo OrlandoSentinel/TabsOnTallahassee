5416f6c1e1d4a977c3f3f556bd912df347cf7bdc
erinspace <erin.braswell@gmail.com>
2015-09-27 22:48:45 -0400

    Add some user preferences to admin panel

--- preferences/admin.py
+++ preferences/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Preferences, PersonFollow, TopicFollow, LocationFollow
+
+admin.site.register(Preferences)
+admin.site.register(PersonFollow)
