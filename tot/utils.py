2bfeedac7bc4570262c4b6a1cd4775dad7642780
erinspace <erin.braswell@gmail.com>
2015-09-28 19:05:19 -0400

    Use new helper method

--- tot/utils.py
+++ tot/utils.py
@@ -1,11 +1,13 @@
 
 
-from opencivicdata.models.people_orgs import Person
+from opencivicdata.models.people_orgs import Person, Organization
 
 
 def get_current_people(position):
 
     if position == 'senator':
-        return Person.objects.filter(memberships__organization__name='Florida Senate')
+        return Organization.objects.get(name='Florida Senate').get_current_members()
+        # return Person.objects.filter(memberships__organization__name='Florida Senate')
     if position == 'representative':
-        return Person.objects.filter(memberships__organization__name='Florida House of Representatives')
+        return Organization.objects.get(name='Florida House of Representatives').get_current_members()
+        # return Person.objects.filter(memberships__organization__name='Florida House of Representatives')
