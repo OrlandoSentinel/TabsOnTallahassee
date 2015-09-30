aca235d06db08f93b130a2b3aa9b3f00ff3c4c00
erinspace <erin.braswell@gmail.com>
2015-09-29 21:01:54 -0400

    Update utils with saving preferences for display

--- tot/utils.py
+++ tot/utils.py
@@ -7,7 +7,19 @@ def get_current_people(position):
 
     if position == 'senator':
         return Organization.objects.get(name='Florida Senate').get_current_members()
-        # return Person.objects.filter(memberships__organization__name='Florida Senate')
     if position == 'representative':
         return Organization.objects.get(name='Florida House of Representatives').get_current_members()
-        # return Person.objects.filter(memberships__organization__name='Florida House of Representatives')
+
+
+def mark_selected(items, items_followed):
+    selected_items = []
+    for item in items:
+        item_dict = {}
+        item_dict['item'] = item
+        if item in items_followed:
+            item_dict['selected'] = True
+        else:
+            item_dict['selected'] = False
+        selected_items.append(item_dict)
+
+    return selected_items
