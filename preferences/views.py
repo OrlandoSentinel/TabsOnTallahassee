48d84bd52b7f1540db57cb1986714d532604b024
erinspace <erin.braswell@gmail.com>
2015-09-29 21:00:26 -0400

    Save reps, senators, locations and subjects in form

--- preferences/views.py
+++ preferences/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from django.db import transaction
 # from django.views.generic import TemplateView
 
-from tot.utils import get_current_people
+from tot.utils import get_current_people, mark_selected
 
 from bills.utils import get_all_subjects, get_all_locations
 
 from registration.forms import RegistrationFormUniqueEmail
 from registration.backends.default.views import RegistrationView
 
-from preferences.models import PersonFollow
+from preferences.models import PersonFollow, LocationFollow, TopicFollow
 
 from opencivicdata.models.people_orgs import Person
 
@@ -25,9 +25,17 @@ def user_preferences(request):
 
     senators = get_current_people(position='senator')
     representatives = get_current_people(position='representative')
-
     locations = get_all_locations()
     subjects = get_all_subjects()
+
+    people_followed = [individual.person for individual in PersonFollow.objects.filter(user=user)]
+    subjects_followed = [subject.topic for subject in TopicFollow.objects.filter(user=user)]
+    locations_followed = [location.location for location in LocationFollow.objects.filter(user=user)]
+
+    selected_reps = mark_selected(representatives, people_followed)
+    selected_senators = mark_selected(senators, people_followed)
+    selected_subjects = mark_selected(subjects, subjects_followed)
+    selected_locations = mark_selected(locations, locations_followed)
 
     if request.method == 'POST':
         with transaction.atomic():
@@ -35,20 +43,20 @@ def user_preferences(request):
             for senator in request.POST.getlist('senators'):
                 PersonFollow.objects.create(user=user, person_id=senator)
             for representative in request.POST.getlist('representatives'):
-                PersonFollow.objects.create(user=user, person_id=representitive)
+                PersonFollow.objects.create(user=user, person_id=representative)
             for location in request.POST.getlist('locations'):
-                PersonFollow.objects.create(user=user, person_id=representitive)
+                LocationFollow.objects.create(user=user, location=location)
             for subject in request.POST.getlist('subjects'):
-                PersonFollow.objects.create(user=user, person_id=representitive)
+                TopicFollow.objects.create(user=user, topic=subject)
 
     return render(
         request,
         'preferences/preferences.html',
         {
             'user': user,
-            'senators': senators,
-            'representatives': representatives,
-            'locations': locations,
-            'subjects': subjects
+            'senators': selected_senators,
+            'representatives': selected_reps,
+            'locations': selected_locations,
+            'subjects': selected_subjects
          }
     )
