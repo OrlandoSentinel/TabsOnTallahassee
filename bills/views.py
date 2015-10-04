f6f7465ecd176510aa5edf09846c6c724c382275
erinspace <erin.braswell@gmail.com>
2015-10-04 14:54:30 -0400

    Start of a bill filter by subject

--- bills/views.py
+++ bills/views.py
@@ -6,9 +6,14 @@ from bills.utils import get_all_subjects, get_all_locations
 from opencivicdata.models import Bill
 
 def bill_list(request):
-    all_bills = Bill.objects.all()
 
     subjects = get_all_subjects()
+
+    if request.POST.getlist('bill_subjects'):
+        filter_subjects = request.POST.getlist('bill_subjects')
+        all_bills = Bill.objects.filter(subject__in=filter_subjects)
+    else:
+        all_bills = Bill.objects.all()
 
     details = []
     for bill in all_bills:
@@ -31,7 +36,10 @@ def bill_list(request):
 
         details.append(bill_detail)
 
-    # import ipdb; ipdb.set_trace()
+        if request.method == 'POST':
+            with transaction.atomic():
+                filter_subjects = request.POST.getlist('bill_subjects')
+            return redirect('.')
 
     return render(
         request,
