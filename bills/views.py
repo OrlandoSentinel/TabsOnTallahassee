886b810596f7b37ac70923ca2bb942dd7c5b8f81
erinspace <erin.braswell@gmail.com>
2015-10-09 04:20:24 -0400

    Make bill parsing functions more generic, add view for pref list

--- bills/views.py
+++ bills/views.py
@@ -6,13 +6,16 @@ from django.shortcuts import render, redirect
 from tot import settings
 from preferences.views import _mark_selected
 from bills.utils import get_all_subjects, get_all_locations
+from preferences.models import PersonFollow, TopicFollow, LocationFollow
 
 from opencivicdata.models import Bill, LegislativeSession
 
 current_session = LegislativeSession.objects.get(name=settings.CURRENT_SESSION)
 
-def bill_list(request):
+all_letters = string.ascii_lowercase
 
+def bill_list(request):
+    alphalist = True
     subjects = get_all_subjects()
 
     if request.POST.getlist('bill_subjects'):
@@ -24,7 +27,7 @@ def bill_list(request):
 
     subjects = _mark_selected(subjects, filter_subjects)
 
-    bills = organize_bill_info(all_bills)
+    bills = organize_bill_info(all_bills=all_bills, sorter='subject')
 
     real_bills = []
     for keyword,bill_list in bills.items():
@@ -35,11 +38,50 @@ def bill_list(request):
     return render(
         request,
         'bills/all.html',
-        {'bills': items, 'subjects': subjects, 'current_session': current_session.name, 'letters': string.ascii_lowercase}
+        {'bills': items, 'subjects': subjects, 'current_session': current_session.name, 'letters': all_letters, 'alphalist': alphalist}
     )
 
 
-def organize_bill_info(all_bills):
+def latest_bill_activity(request):
+    alphalist = False
+    topic_bills = []
+    location_bills = []
+    if not request.user.is_anonymous():
+        user = request.user
+
+        # people_followed = PersonFollow.objects.filter(user=user)
+        topics_followed = [item.topic for item in TopicFollow.objects.filter(user=user)]
+        locations_followed = [item.location for item in LocationFollow.objects.filter(user=user)]
+
+        #  TODO - how do I make this not a loop?
+        for subject in topics_followed:
+            topic_bills += Bill.objects.filter(legislative_session=current_session, subject__contains=[subject])
+        
+        #  TODO - how do I actually do this query?
+        # for location in locations_followed:
+        #     location_bills += Bill.objects.filter(legislative_session=current_session, extras__contains=[location])
+        location_bills = Bill.objects.filter(legislative_session=current_session, subject__contains=['insurance'])
+
+        organized_subjects = organize_bill_info(topic_bills)
+        organized_locations = organize_bill_info(all_bills=location_bills, sorter='location')
+
+        bills = organized_subjects.copy()
+        bills.update(organized_locations)
+
+        real_bills = []
+        for keyword,bill_list in bills.items():
+            real_bills.append({'name': keyword, 'sorter': keyword[0].lower(), 'bills': bill_list})
+
+        items = sorted(real_bills, key = lambda x: x["name"])
+
+    return render(
+        request,
+        'bills/all.html',
+        {'bills': items, 'current_session': current_session.name, 'letters': all_letters, 'alphalist': alphalist}
+    )
+
+
+def organize_bill_info(all_bills, sorter='subject'):
     bills = {}
     for bill in all_bills:
         bill_locations = bill.extras['places']
@@ -64,10 +106,17 @@ def organize_bill_info(all_bills):
                 'primary': sponsorship.primary
             })
 
-        for subject in bill.subject:
-            if subject in bills.keys():
-                bills[subject].append(bill_detail)
-            else:
-                bills[subject] = [bill_detail]
+        if sorter == 'subject':
+            for subject in bill.subject:
+                if subject in bills.keys():
+                    bills[subject].append(bill_detail)
+                else:
+                    bills[subject] = [bill_detail]
+        if sorter == 'location':
+            for location in bill.extras['places']:
+                if location in bills.keys():
+                    bills[location].append(bill_detail)
+                else:
+                    bills[location] = [bill_detail]
 
     return bills
