5f76920fe9fc16752b9adbefe383d2c989b9b17e
James Turk <james.p.turk@gmail.com>
2015-10-18 21:54:24 -0400

    a bunch of person tests

--- api/tests.py
+++ api/tests.py
@@ -1,5 +1,6 @@
-from django.test import TestCase
 import json
+from django.test import TestCase
+from opencivicdata.models import Person, Organization, Membership
 
 
 class ApiTests(TestCase):
@@ -18,7 +19,7 @@ class ApiTests(TestCase):
         # check legislative_sessions isn't there by default
         assert 'legislative_sessions' not in data['data'][0]['attributes']
 
-        # ensure self URL is correct
+        # ensure self URL is correct (was being masked by .url attribute)
         url = data['data'][0]['links']['self']
         assert '/api/ocd-jurisdiction/country:us/state:fl/government/' in url
 
@@ -33,6 +34,48 @@ class ApiTests(TestCase):
     def test_person_list(self):
         resp = self._api('people/?')
         self.assertEqual(resp.status_code, 200)
+        data = json.loads(resp.content.decode('utf8'))
+        assert len(data['data']) == 50
+
+    def test_person_list_by_name(self):
+        resp = self._api('people/?name=John')
+        self.assertEqual(resp.status_code, 200)
+        data = json.loads(resp.content.decode('utf8'))
+        assert len(data['data']) == 4
+
+    def test_person_list_by_member_of(self):
+        resp = self._api('people/?member_of=Republican')
+        data = json.loads(resp.content.decode('utf8'))
+        assert data['meta']['pagination']['count'] == 107
+
+    def test_person_list_by_member_of_name(self):
+        resp = self._api('people/?member_of=Republican&name=John')
+        data = json.loads(resp.content.decode('utf8'))
+        assert data['meta']['pagination']['count'] == 3
+
+    def test_person_list_by_former_member_of(self):
+        resp = self._api('people/?ever_member_of=Republican')
+        data = json.loads(resp.content.decode('utf8'))
+        assert data['meta']['pagination']['count'] == 107
+
+        # add former membership to republican party to democrat
+        r = Organization.objects.get(name='Republican')
+        Membership.objects.create(person_id='ocd-person/0446be69-7504-417d-97c1-136589908ee5',
+                                  organization=r, end_date='2000')
+
+        # ever_member_of now returns 108 & member_of still returns 107
+        resp = self._api('people/?ever_member_of=Republican')
+        data = json.loads(resp.content.decode('utf8'))
+        assert data['meta']['pagination']['count'] == 108
+        resp = self._api('people/?member_of=Republican')
+        data = json.loads(resp.content.decode('utf8'))
+        assert data['meta']['pagination']['count'] == 107
+
+    def test_person_list_by_lat_lon(self):
+        resp = self._api('people/?latitude=39&longitude=-88')
+        data = json.loads(resp.content.decode('utf8'))
+        #assert data['meta']['pagination']['count'] == 2
+        # TODO get lat long working
 
     def test_bill_list(self):
         resp = self._api('bills/?')
