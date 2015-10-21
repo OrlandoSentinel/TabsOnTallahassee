80672753e84f603d6892d1ca48f283e46c0b539d
James Turk <james.p.turk@gmail.com>
2015-10-21 17:36:16 -0400

    add docs for demo

--- tot/settings.py
+++ tot/settings.py
@@ -28,6 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.contrib.flatpages',
     'opencivicdata.apps.BaseConfig',
     'glossary',
     'boundaries',
--- tot/urls.py
+++ tot/urls.py
@@ -20,6 +20,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from preferences.views import EmailRegistrationView, user_preferences
 from bills.views import bill_list, latest_bill_activity
 from home import views
+from django.contrib.flatpages import views as flatpages_views
 
 
 admin.site.site_header = 'Tabs on Tallahassee Admin'
@@ -37,6 +38,9 @@ urlpatterns = [
     url(r'^bills/', bill_list, name='bills_list'),
     url(r'^latest/', latest_bill_activity, name='latest_bill_activity'),
     url(r'^admin/', include('opencivicdata.admin.urls')),
+
+    # flatpages
+    url(r'^api/$', flatpages_views.flatpage, {'url': '/api/'}, name='api-docs'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
