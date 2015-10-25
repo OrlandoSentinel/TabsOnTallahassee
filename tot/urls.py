bfc8cdabadbf5dba06436590d3e075ec0762aa6f
James Turk <james.p.turk@gmail.com>
2015-10-25 19:48:52 -0400

    move home/about to flatpages

--- tot/urls.py
+++ tot/urls.py
@@ -33,13 +33,13 @@ urlpatterns = [
     url(r'^accounts/register/', EmailRegistrationView.as_view(), name='registration_register'),
     url(r'^preferences/', user_preferences, name='preferences'),
     url(r'^set_user_latlon/', set_user_latlon, name='set_user_latlon'),
-    url(r'^$', 'home.views.index'),
-    url(r'^about/', 'home.views.about'),
     url(r'^bills/', bill_list, name='bills_list'),
     url(r'^latest/', latest_bill_activity, name='latest_bill_activity'),
     url(r'^admin/', include('opencivicdata.admin.urls')),
 
     # flatpages
+    url(r'^$', flatpages_views.flatpage, {'url': '/'}, name='home'),
+    url(r'^about/$', flatpages_views.flatpage, {'url': '/about/'}, name='about'),
     url(r'^api/$', flatpages_views.flatpage, {'url': '/api/'}, name='api-docs'),
 ]
 
