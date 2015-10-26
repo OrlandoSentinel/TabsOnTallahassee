31a186bb91023c7b6e8beca62d225dc64dcd90b8
erinspace <erin.braswell@gmail.com>
2015-10-26 19:58:21 -0400

    Save and pass the address to the session

--- home/views.py
+++ home/views.py
@@ -21,6 +21,7 @@ def about(request):
 def find_legislator(request):
     senator = request.session.get('sen_from_address')
     representative = request.session.get('rep_from_address')
+    address = request.session.get('address')
 
     if senator and representative:
         senator = json.loads(senator)
@@ -29,7 +30,7 @@ def find_legislator(request):
     return render(
         request,
         'home/find_legislator.html',
-        {'address_senator': senator, 'address_representative': representative}
+        {'address_senator': senator, 'address_representative': representative, 'address': address}
     )
 
 
@@ -38,6 +39,7 @@ def get_latlon(request):
     if request.is_ajax():
         lat = request.GET.get('lat', '')
         lon = request.GET.get('lon', '')
+        address = request.GET.get('address', '')
 
         api_resp = requests.get(
             settings.DOMAIN + '/api/people/?latitude={}&longitude={}&apikey={}'.format(
@@ -60,5 +62,6 @@ def get_latlon(request):
         else:
             request.session['sen_from_address'] = request.session['rep_from_address'] = json.dumps({'name': 'none found'})
 
+    request.session['address'] = address
     request.session.modified = True
     return redirect(find_legislator)
