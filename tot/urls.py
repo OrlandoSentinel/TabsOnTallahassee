35354c305220cf9424b9cc5bf1f71e251e6c6017
erinspace <erin.braswell@gmail.com>
2015-10-26 20:35:16 -0400

    Include new legislators pages in tot urls

--- tot/urls.py
+++ tot/urls.py
@@ -3,7 +3,6 @@ from django.conf.urls import url, include
 from django.contrib.flatpages import views as flatpages_views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from home.views import index, find_legislator, about, get_latlon
 from bills.views import bill_list, latest_bill_activity
 from preferences.views import EmailRegistrationView, user_preferences, set_user_latlon
 
@@ -19,10 +18,7 @@ urlpatterns = [
     url(r'^accounts/register/', EmailRegistrationView.as_view(), name='registration_register'),
     url(r'^preferences/', user_preferences, name='preferences'),
     url(r'^set_user_latlon/', set_user_latlon, name='set_user_latlon'),
-    url(r'^get_latlon/', get_latlon),
-    url(r'^$', index),
-    url(r'^find_legislator/', find_legislator),
-    url(r'^about/', about),
+    url('^', include('legislators.urls')),
     url(r'^bills/', bill_list, name='bills_list'),
     url(r'^latest/', latest_bill_activity, name='latest_bill_activity'),
     url(r'^admin/', include('opencivicdata.admin.urls')),
