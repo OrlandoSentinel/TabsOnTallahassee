e6590a916743b5c473e87d4eb8398c3018c3f674
James Turk <james.p.turk@gmail.com>
2015-11-11 22:23:27 -0500

    enable template cache loader, #46

--- tot/settings.py
+++ tot/settings.py
@@ -72,6 +72,12 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
+            'loaders': [
+                ('django.template.loaders.cached.Loader', [
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
+                ]),
+            ],
         },
     },
 ]
