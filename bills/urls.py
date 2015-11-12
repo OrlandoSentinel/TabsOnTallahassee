94e85fb24a9b2c327094b880e05251ffb00c1335
erinspace <erin.braswell@gmail.com>
2015-11-11 20:30:27 -0500

    Add urls for list by topic and by location

--- bills/urls.py
+++ bills/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^list/', views.bill_list),
+    url(r'^by_topic/', views.bill_list_by_topic),
+    url(r'^by_location', views.bill_list_by_location),
     url(r'^latest_activity/', views.latest_bill_activity),
     url(r'^latest/', views.latest_bill_actions),
     url(r'^detail/(?P<bill_id>(.*))/$', views.bill_detail, name='bill_detail'),
