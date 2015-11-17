faed82947209b34ccb4063e8244a9da019fa52a2
erinspace <erin.braswell@gmail.com>
2015-11-16 23:53:27 -0500

    Update bills added by current session

--- bills/urls.py
+++ bills/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^by_topic/', views.bill_list_by_topic),
     url(r'^by_location', views.bill_list_by_location),
-    url(r'^latest_activity/', views.latest_bill_activity),
-    url(r'^latest/', views.latest_bill_actions),
+    url(r'^current_session/', views.bill_list_current_session),
+    url(r'^latest_activity/', views.bill_list_latest),
     url(r'^detail/(?P<bill_session>(.*))/(?P<bill_identifier>(.*))/$', views.bill_detail, name='bill_detail'),
 ]
