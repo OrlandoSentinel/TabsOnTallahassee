1ef75a98efed2cdb211a9fae33ecbf807e272118
erinspace <erin.braswell@gmail.com>
2015-11-18 00:08:59 -0500

    Add new url and link on base to sort by legislator

--- bills/urls.py
+++ bills/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import url
 urlpatterns = [
     url(r'^by_topic/', views.bill_list_by_topic),
     url(r'^by_location', views.bill_list_by_location),
+    url(r'^by_legislator', views.bill_list_by_legislator),
     url(r'^current_session/', views.bill_list_current_session),
     url(r'^latest_activity/', views.bill_list_latest),
     url(r'^detail/(?P<bill_session>(.*))/(?P<bill_identifier>(.*))/$', views.bill_detail, name='bill_detail'),
