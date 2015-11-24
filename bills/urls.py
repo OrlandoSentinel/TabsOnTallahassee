60c035210290981f178a9346467b6dfa674e63b8
erinspace <erin.braswell@gmail.com>
2015-11-23 19:27:45 -0500

    Use url template names instead of paths

--- bills/urls.py
+++ bills/urls.py
@@ -3,10 +3,10 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^by_topic/', views.bill_list_by_topic),
-    url(r'^by_location', views.bill_list_by_location),
-    url(r'^by_legislator', views.bill_list_by_legislator),
-    url(r'^current_session/', views.bill_list_current_session),
-    url(r'^latest_activity/', views.bill_list_latest),
+    url(r'^by_topic/', views.bill_list_by_topic, name='by_topic'),
+    url(r'^by_location', views.bill_list_by_location, name='by_location'),
+    url(r'^by_legislator', views.bill_list_by_legislator, name='by_legislator'),
+    url(r'^current_session/', views.bill_list_current_session, name='current_session'),
+    url(r'^latest_activity/', views.bill_list_latest, name='latest'),
     url(r'^detail/(?P<bill_session>(.*))/(?P<bill_identifier>(.*))/$', views.bill_detail, name='bill_detail'),
 ]
