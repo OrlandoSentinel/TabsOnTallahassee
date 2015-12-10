a9f238294cd9033e50a0cd4e97f0702bd1dc55fb
James Turk <james.p.turk@gmail.com>
2015-12-09 20:01:27 -0500

    newrelic

--- tot/wsgi.py
+++ tot/wsgi.py
@@ -6,8 +6,16 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/dev/howto/deployment/wsgi/
 """
-
 import os
+
+try:
+    import newrelic.agent
+    newrelic.agent.initialize(os.path.join(
+        os.path.dirname(__file__),
+        'newrelic.ini'
+    ))
+except Exception as e:
+    print("newrelic couldn't be initialized:", e)
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
