b5496ac6e643d1a9e0ea8de932b2b369773db83f
James Turk <james.p.turk@gmail.com>
2015-12-13 15:36:54 -0500

    add glossary js

---
+++ static/js/glossary.js
@@ -0,0 +1,16 @@
+function replace() {
+  var watch_words = {
+    'Bill': 'a thing',
+    'Activity': 'an action',
+  };
+  function span_wrap(text) {
+    return ' <span class="glossary" title="' + watch_words[text] + '">' + text + '</span> ';
+  }
+  var keys = $.map(watch_words, function(element,index) {return index});
+  var re = RegExp('\\b(' + keys.join('|') + ')\\b', "g");
+  $('div').html(function(i, html) {
+    return html.replace(re, span_wrap);
+  });
+};
+
+$(document).ready(replace);
