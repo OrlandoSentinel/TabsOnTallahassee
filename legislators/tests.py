79862c0a79616c3965d90415b5ea723c119fb7b0
erinspace <erin.braswell@gmail.com>
2015-12-14 21:45:30 -0500

    clean up tests

--- legislators/tests.py
+++ legislators/tests.py
@@ -25,20 +25,3 @@ class LegislatorViewTests(StaticLiveServerTestCase):
             'legislator_detail',
             args=['ocd-person/016cf1f8-94ea-49a2-b1df-2701cee64ad2']))
         self.assertEqual(response.status_code, 200)
-
-    def test_detail_page_add_legislator_adds_to_preferences(self):
-        # check to see if user preferences are blank
-
-        # go to legislator detail page
-        response = self.client.get(reverse(
-            'legislator_detail',
-            args=['ocd-person/016cf1f8-94ea-49a2-b1df-2701cee64ad2']))
-
-        # add legislator on page to user preferneces
-
-        # check to see if user preferences contains representative selected
-        #TODO
-        pass
-
-    def test_some_test_stuff(self):
-        import ipdb; ipdb.set_trace()
