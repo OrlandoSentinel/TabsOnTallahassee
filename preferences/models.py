20a0e0ccb64576c45bee913c1ed6be5115d0d535
James Turk <james.p.turk@gmail.com>
2016-02-15 10:30:22 -0500

    add migration w/ new email prefs

--- preferences/models.py
+++ preferences/models.py
@@ -3,6 +3,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from opencivicdata.models.people_orgs import Person
 
+EMAIL_FREQUENCIES = (
+    ('N', 'Never'),
+    ('D', 'Daily'),
+    ('W', 'Weekly'),
+)
+
+EMAIL_TYPES = (
+    ('H', 'HTML'),
+    ('T', 'Plain Text'),
+)
+
 
 class Preferences(models.Model):
     user = models.OneToOneField(User, related_name='preferences')
@@ -12,6 +23,12 @@ class Preferences(models.Model):
     rep_from_address = models.CharField(max_length=255, blank=True, null=True)
     sen_from_address = models.CharField(max_length=255, blank=True, null=True)
     apikey = models.UUIDField(default=uuid.uuid4)
+
+    # email preferences
+    email_frequency = models.CharField(max_length=1, choices=EMAIL_FREQUENCIES,
+                                       default='N')
+    email_type = models.CharField(max_length=1, choices=EMAIL_TYPES,
+                                  default='T')
 
 
 class PersonFollow(models.Model):
